abb0d191c6d74805c37597bcd989e05d18d2b67f
Richard Choi <justcodeitrich@gmail.com>
2022-04-22 16:50:10 -0400

    step 3

--- oop_tictactoe.rb
+++ oop_tictactoe.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Board
   INITIAL_MARKER = " "
 
@@ -9,31 +10,39 @@ class Board
   def get_square_at(key)
     @squares[key] 
   end
+
+  def set_square_at(key,marker)
+    @squares[key].marker = marker
+  end
 end
 
 class Square
+  attr_accessor :marker 
+
   def initialize(marker)
     @marker = marker 
   end
-  
+
   def to_s
     @marker
   end
 end
 
 class Player
-  def initialize
-  end
+  attr_reader :marker 
 
-  def mark
+  def initialize(marker)
+    @marker = marker 
   end
 end
 
 class TTTGame
-  attr_reader :board 
+  attr_reader :board, :human, :computer 
 
   def initialize
     @board = Board.new
+    @human = Player.new("X")
+    @computer = Player.new("O")
   end
 
   def display_welcome_message
@@ -59,16 +68,30 @@ class TTTGame
     puts "     |     |"
     puts ""
   end
+
+  def human_moves
+    puts "Choose a square between 1 and 9:"
+    square = nil 
+    loop do 
+      square = gets.chomp.to_i 
+      break if (1..9).include?(square)
+      puts "Sorry that's not a valid choice."
+    end
+    binding.pry
+    board.set_square_at(square, @human.marker)
+  end
   
   def play
     display_welcome_message
     loop do
       display_board
+      
+      human_moves
+      display_board
       break
-      first_player_moves
       break if someone_won? || board_full?
 
-      second_player_moves
+      computer_moves
       break if someone_won? || board_full?
     end
     # display_result
