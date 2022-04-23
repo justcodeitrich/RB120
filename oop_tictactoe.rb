6b150d44c6d832db32b3087ea9ca13f692cb616b
Richard Choi <justcodeitrich@gmail.com>
2022-04-23 16:40:45 -0400

    step 7

--- oop_tictactoe.rb
+++ oop_tictactoe.rb
@@ -1,6 +1,8 @@
 require 'pry'
 class Board
-  
+  WINNING_LINES = [[1,2,3],[4,5,6],[7,8,9]] + 
+                  [[1,4,7],[2,5,8],[3,6,9]] + 
+                  [[1,5,9],[3,5,7]]
 
   def initialize
     @squares = {}
@@ -21,6 +23,21 @@ class Board
 
   def full? 
     unmarked_keys.empty?
+  end
+
+  def someone_won?
+    !!detect_winner
+  end
+
+  def detect_winner 
+    WINNING_LINES.each do |line| 
+      if @squares[line[0]].marker == TTTGame::HUMAN_MARKER && @squares[line[1]].marker == TTTGame::HUMAN_MARKER && @squares[line[2]].marker == TTTGame::HUMAN_MARKER
+        return TTTGame::HUMAN_MARKER
+      elsif @squares[line[0]].marker == TTTGame::COMPUTER_MARKER && @squares[line[1]].marker == TTTGame::COMPUTER_MARKER && @squares[line[2]].marker == TTTGame::COMPUTER_MARKER
+        return TTTGame::COMPUTER_MARKER
+      end
+    end
+    nil 
   end
 end
 
@@ -103,7 +120,15 @@ class TTTGame
   end
 
   def display_result
-    puts "the board is full!"
+    display_board
+    case board.detect_winner
+    when human.marker 
+      puts "You won!"
+    when computer.marker 
+      puts "Computer won!"
+    else 
+      puts "It's a tie!"
+    end
   end
   
   def play
@@ -111,12 +136,11 @@ class TTTGame
     display_board
     loop do
       human_moves
-      break if board.full?
-      # break if someone_won? || board_full?
+      break if board.someone_won? || board.full?
+
       computer_moves
-      break if board.full?
+      break if board.someone_won? || board.full?
       display_board
-      # break if someone_won? || board_full?
     end
     display_result
     display_goodbye_message
