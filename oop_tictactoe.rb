137b87efe85ecb8e45ab141de97d28606f68ad83
Richard Choi <justcodeitrich@gmail.com>
2022-04-25 16:37:17 -0400

    clear screen + winning marker

--- oop_tictactoe.rb
+++ oop_tictactoe.rb
@@ -26,7 +26,7 @@ class Board
   end
 
   def someone_won?
-    !!detect_winner
+    !!winning_marker
   end
 
   def count_human_marker(squares)
@@ -37,7 +37,7 @@ class Board
     squares.map(&:marker).count(TTTGame::COMPUTER_MARKER)
   end
 
-  def detect_winner 
+  def winning_marker 
     WINNING_LINES.each do |line| 
       if count_human_marker(@squares.values_at(*line)) == 3
         return TTTGame::HUMAN_MARKER
@@ -98,8 +98,7 @@ class TTTGame
     puts "Thanks for playing. Goodbye!"
   end
 
-  def display_board(clear_screen: true)
-    clear if clear_screen
+  def display_board
     puts "You are #{human.marker}. Computer is #{computer.marker}."
     puts ""
     puts "     |     |"
@@ -132,8 +131,8 @@ class TTTGame
   end
 
   def display_result
-    display_board
-    case board.detect_winner
+    clear_screen_and_display_board
+    case board.winning_marker
     when human.marker 
       puts "You won!"
     when computer.marker 
@@ -157,19 +156,24 @@ class TTTGame
   def clear
     system "clear" 
   end
+
+  def clear_screen_and_display_board
+    clear 
+    display_board
+  end
   
   def play
     clear
     display_welcome_message
     loop do 
-    display_board(clear_screen: false)
+    display_board
       loop do
         human_moves
         break if board.someone_won? || board.full?
 
         computer_moves
         break if board.someone_won? || board.full?
-        display_board
+        clear_screen_and_display_board
       end
       display_result
       break unless play_again?
