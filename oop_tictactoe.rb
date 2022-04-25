a7658d631b41426b74815211591e12977b9a87a2
Richard Choi <justcodeitrich@gmail.com>
2022-04-25 15:30:40 -0400

    step 9

--- oop_tictactoe.rb
+++ oop_tictactoe.rb
@@ -6,7 +6,7 @@ class Board
 
   def initialize
     @squares = {}
-    (1..9).each {|key| @squares[key] = Square.new}
+    reset
   end
 
   def get_square_at(key)
@@ -46,6 +46,10 @@ class Board
       end
     end
     nil 
+  end
+
+  def reset 
+    (1..9).each {|key| @squares[key] = Square.new}
   end
 end
 
@@ -94,8 +98,8 @@ class TTTGame
     puts "Thanks for playing. Goodbye!"
   end
 
-  def display_board
-    system('clear')
+  def display_board(clear=true)
+    system('clear') if clear 
     puts "You are #{human.marker}. Computer is #{computer.marker}."
     puts ""
     puts "     |     |"
@@ -138,19 +142,38 @@ class TTTGame
       puts "It's a tie!"
     end
   end
+
+  def play_again?
+    answer = nil
+    loop do 
+      puts "Would you like to play again? (y/n)"
+      answer = gets.chomp.downcase
+      break if %w(y n).include? answer 
+      puts "Sorry, that's not a valid answer."
+    end
+    answer == "y"
+  end 
   
   def play
+    system "clear"
     display_welcome_message
-    display_board
-    loop do
-      human_moves
-      break if board.someone_won? || board.full?
+    loop do 
+    display_board(false)
+      loop do
+        human_moves
+        break if board.someone_won? || board.full?
 
-      computer_moves
-      break if board.someone_won? || board.full?
-      display_board
+        computer_moves
+        break if board.someone_won? || board.full?
+        display_board
+      end
+      display_result
+      break unless play_again?
+      board.reset
+      system "clear"
+      puts "Let's play again"
+      puts ""
     end
-    display_result
     display_goodbye_message
   end
 end
