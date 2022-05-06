f769788b6f1999ca5ed45c47eb934256453919ec
Richard Choi <justcodeitrich@gmail.com>
2022-05-06 15:11:40 -0400

    finished rough total game sequence

--- twentyone.rb
+++ twentyone.rb
@@ -33,14 +33,14 @@ class Participant
   end
 
   def calculate_hand_value
-    @total_hand_value = 0 
+    @total_hand_value = 0
     hand.each do |cards|
       @total_hand_value += card_to_value(cards)
     end
   end
 
   def display_hand_total
-    puts "Your cards total to #{@total_hand_value} "
+    puts "#{name}'s cards total to #{@total_hand_value} "
   end
 
   def card_to_value(card)
@@ -48,14 +48,14 @@ class Participant
     case face
     when "J" then 10
     when "Q" then 10
-    when "K" then 10 
+    when "K" then 10
     when "A" then ace_to_value(face)
-    else face 
+    else face
     end
     # returns a number
   end
 
-  def ace_to_value(face)
+  def ace_to_value
     if @total_hand_value + 11 > 21
       1
     else
@@ -73,15 +73,6 @@ class Participant
   def bust?
     @total_hand_value > 21
   end
-
-  # cards
-  # hit
-    # total value of hand
-    # converts value of face cards to numbers
-    # accounts for aces - checks to see if the ace was 11, if it causes player to bust
-    # if bust, then ace is valued at 1
-  # stay
-  # busted?
 end
 
 class Dealer < Participant
@@ -91,15 +82,13 @@ class Dealer < Participant
     revealed_card = hand[0]
     puts "#{name} has a #{revealed_card[0]} of #{revealed_card[1]} and one hidden card."
   end
-  
+
   def hand_total_below_minimum?
-    binding.pry
     total_hand_value < DEALER_TOTAL_MINIMUM
   end
 end
 
 class Player < Participant
-
 end
 
 class Game
@@ -107,9 +96,10 @@ class Game
 
   def initialize
     @deck = Deck.new
-    @player = Player.new("Tom")
-    @dealer = Dealer.new("Jerry")
+    @player = Player.new("Player")
+    @dealer = Dealer.new("Dealer")
     @player_busted = false
+    @dealer_busted = false
   end
 
   def deal_two_cards_to_all_players
@@ -123,10 +113,10 @@ class Game
     game_setup
     player_sequence
     dealer_sequence
-    # method to check if player busted. If player busted, go to end game
+    determine_winner
   end
-  
-  def game_setup 
+
+  def game_setup
     deck.shuffle_cards
     deal_two_cards_to_all_players
     player.show_cards_in_hand
@@ -135,22 +125,21 @@ class Game
     player.display_hand_total
     dealer.show_one_card_of_hand
   end
-  # deal initial hands
-  # player loop
-    # show hand
-    # calculate value
-    # display hand value
-    # ask hit or stay
-    # hit until stay or bust
-    # break if stay
-  # dealer loop
 
   def hit
     deck.deal_one_card
   end
 
+  def player_busted?
+    @player_busted
+  end
+
+  def dealer_busted?
+    @dealer_busted
+  end
+
   def player_sequence
-    loop do 
+    loop do
       answer = ask_hit_or_stay
       break if answer == "s" || answer == "stay"
       if answer == "h" || answer == "hit"
@@ -159,8 +148,8 @@ class Game
         player.calculate_hand_value # calculate new total
         player.display_hand_total # display total value
         if player.bust?
-          puts "sorry you lose with value of #{player.total_hand_value}"
-          break 
+          @player_busted = true
+          break
           # sorry you lose with a hand over 21 - break out of game to the end
         end
       end
@@ -168,18 +157,20 @@ class Game
     end
   end
 
-  def dealer_sequence 
+  def dealer_sequence
+    return if player_busted?
     dealer.show_cards_in_hand
     dealer.display_hand_total
-    loop do 
+    loop do
       if dealer.bust?
+        @dealer_busted = true
         puts "dealer busted with a hand of #{dealer.total_hand_value}!"
         break
       elsif dealer.hand_total_below_minimum?
         dealer.hand << hit
         dealer.show_cards_in_hand
         dealer.calculate_hand_value
-      else 
+      else
         puts "Dealer stays with a final hand is a value of #{dealer.total_hand_value}."
         break
       end
@@ -197,16 +188,17 @@ class Game
     answer
   end
 
-
-
-  # deal hands
-  # display player's hands and only one card of dealer hand
-  # ask player to hit or stay
-  # repeat until player busts or stays
-  # reveal dealers full hand
-  # dealer hits if hand value is less than 17, stays if 17 and over
-  # repeat until dealer busts or is within 17 - 21 range
-  # display to player who wins and their winning hand
+  def determine_winner
+    return puts "Sorry you busted! You lose" if player_busted?
+    return puts "Dealer busted! You win!" if dealer_busted?
+    if player.total_hand_value == dealer.total_hand_value
+      puts "It's a tie!"
+    elsif player.total_hand_value > dealer.total_hand_value
+      puts "Player wins!"
+    else
+      puts "Dealer wins!"
+    end
+  end
 end
 
 game = Game.new
